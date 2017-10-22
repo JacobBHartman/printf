9412a1466299b5365770bd70f939f611efc6c545
JacobBHartman <209@holbertonschool.com>
2017-10-22 21:39:35 +0000

    initial commit, we are starting our project

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _printf - a proto-printf function
+ * @format: the format specifier string
+ *
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	
+}
---
+++ holberton.h
@@ -0,0 +1,12 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/* libraries and header files */
+#include <limits.h>
+#include <stdio.h>
+
+/* structure declarations here */
+
+/* function prototypes here */
+
+#endif /* _HOLBERTON_H_ */
